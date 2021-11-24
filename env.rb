bdbb61f56c5b0c71e6f840a5b3d7c452ce008210
Gustavo Cardoso da Silveira <gustavo.silveira@softdesign.com.br>
2021-11-24 14:10:33 -0300

    dd

---
+++ env.rb
@@ -0,0 +1,8 @@
+require 'allure-cucumber'
+Cucumber::Core::Test::Step.module_eval do
+   def name
+     return text if text == 'Before hook'
+     return text if text == 'After hook'
+     "#{source.last.keyword}#{text}"
+   end
+end
